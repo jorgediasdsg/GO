f406252e7fe3f3ed26c5163841b1a6040d419018
Jorge Dias <jorgediasdsg@gmail.com>
2025-08-23 09:17:49 -0300

    ADD: Open file

--- 1brc-challenge/main.go
+++ 1brc-challenge/main.go
@@ -1,9 +1,20 @@
 package main
 
+import "os"
+
 type Measurement struct {
 	Name  string
 	Min   float64
 	Max   float64
 	Sum   float64
 	Count int64
+}
+
+func main() {
+	measurements, err := os.Open("measurements.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer measurements.Close()
+
 }
