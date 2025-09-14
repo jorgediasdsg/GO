6b2a5fef7fa9f1332529dfaf8e8176757aad8ca6
JORGE DIAS <jorgediasdsg@gmail.com>
2025-09-14 08:04:41 -0300

    ADD: Send post and redirect url

--- shortener/api/api.go
+++ shortener/api/api.go
@@ -1,35 +1,108 @@
 package api
 
 import (
+	"encoding/json"
+	"log/slog"
+	"math/rand"
 	"net/http"
+	"net/url"
 
-	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
-// DTOs
-type ShortenRequest struct {
-	URL string `json:"url"`
-}
-type ShortenResponse struct {
-	Error    string `json:"error,omitempty"`
-	ShortURL string `json:"short_url,omitempty"`
+func sendJSON(w http.ResponseWriter, resp Response, status int) {
+	w.Header().Set("Content-Type", "application/json")
+	data, err := json.Marshal(resp)
+	if err != nil {
+		slog.Error("failed to marshal json data", "error", err)
+		sendJSON(
+			w,
+			Response{Error: "something went wrong"},
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	w.WriteHeader(status)
+	if _, err := w.Write(data); err != nil {
+		slog.Error("failed to write json data", "error", err)
+		return
+	}
 }
 
-func NewHandler() http.Handler {
+func NewHandler(db map[string]string) http.Handler {
 	r := chi.NewMux()
 
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 
-	r.Post("api/shorten", handlePost)
-	r.Get("/{code}", handleGet)
+	r.Post("/api/shorten", handlePost(db))
+	r.Get("/{code}", handleGet(db))
 
 	return r
 }
 
-func handlePost(w http.ResponseWriter, r *http.Request) {
-
+type PostBody struct {
+	URL string `json:"url"`
 }
-func handleGet(w http.ResponseWriter, r *http.Request) {}
+
+type Response struct {
+	Error string `json:"error,omitempty"`
+	Data  any    `json:"data,omitempty"`
+}
+
+func handlePost(db map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var body PostBody
+		if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+			sendJSON(
+				w,
+				Response{Error: "invalid body"},
+				http.StatusUnprocessableEntity,
+			)
+			return
+		}
+
+		if _, err := url.Parse(body.URL); err != nil {
+			sendJSON(
+				w,
+				Response{Error: "invalid url passed"},
+				http.StatusBadRequest,
+			)
+		}
+
+		code := genCode()
+		db[code] = body.URL
+
+		sendJSON(
+			w,
+			Response{Data: code},
+			http.StatusCreated,
+		)
+	}
+}
+
+const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func genCode() string {
+	const n = 8
+	byts := make([]byte, n)
+	for i := range n {
+		byts[i] = characters[rand.Intn(len(characters))]
+	}
+	return string(byts)
+}
+
+func handleGet(db map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		code := chi.URLParam(r, "code")
+		data, ok := db[code]
+		if !ok {
+			http.Error(w, "url nao encontrada", http.StatusNotFound)
+			return
+		}
+		http.Redirect(w, r, data, http.StatusPermanentRedirect)
+	}
+}
